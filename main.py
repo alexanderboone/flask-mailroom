574ef0e54a795a8323a815335cf8c1c912b951f2
Alexander Boone <alboone@uw.edu>
2020-03-24 18:05:10 -0700

    completed assignment 1 for flask mailroom

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ import os
 import base64
 
 from flask import Flask, render_template, request, redirect, url_for, session
+from passlib.hash import pbkdf2_sha256
 
-from model import Donation 
+from model import User, Donation, Donor
 
 app = Flask(__name__)
+app.secret_key = os.environ.get('SECRET_KEY').encode()
 
 @app.route('/')
 def home():
@@ -15,7 +17,35 @@ def home():
 def all():
     donations = Donation.select()
     return render_template('donations.jinja2', donations=donations)
-    
+
+@app.route('/new-donation/', methods=['GET', 'POST'])
+def new_donation():
+    if 'username' not in session:
+        return redirect(url_for('login'))
+    if request.method == 'POST':
+        donor_name = request.form['donor_name']
+        donation_amt = request.form['donation_amount']
+        Donation(value=donation_amt, donor=Donor.get(Donor.name==donor_name)).save()
+        return redirect(url_for('home'))
+    else:
+        donors = Donor.select()
+        return render_template('new-donation.jinja2', donors=donors)
+
+@app.route('/login/', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        try:
+            user = User.select().where(User.name == request.form["username"]).get()
+        except User.DoesNotExist:
+            user=''
+            error_message="Incorrect username or password."
+
+        if user and pbkdf2_sha256.verify(request.form["password"], user.password):
+            session['username'] = request.form["username"]
+            return redirect(url_for('new_donation'))
+        return render_template('login.jinja2', error=error_message)
+    else:
+        return render_template('login.jinja2')
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6738))
--- setup.py
+++ setup.py
@@ -1,14 +1,18 @@
 import random
 
-from model import db, Donor, Donation 
+from model import db, User, Donor, Donation
+from passlib.hash import pbkdf2_sha256 
 
 db.connect()
 
 # This line will allow you "upgrade" an existing database by
 # dropping all existing tables from it.
-db.drop_tables([Donor, Donation])
+db.drop_tables([User, Donor, Donation])
 
-db.create_tables([Donor, Donation])
+db.create_tables([User, Donor, Donation])
+
+admin = User(name='admin', password=pbkdf2_sha256.hash('password'))
+admin.save()
 
 alice = Donor(name="Alice")
 alice.save()
